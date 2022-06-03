c0a50b6ba31affd94b0929c9b235dafab75c3c37
Adwait Kesharwani <adwait1291@gmail.com>
2022-06-03 22:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ st.markdown(html_temp,unsafe_allow_html=True)
 #---------------Input--------------#
 
 question = st.text_input("Enter any question"," ")
+question = str(question)
 question = np.array([question])
 question = pd.Series(question)
 question =vect.transform(question)     
